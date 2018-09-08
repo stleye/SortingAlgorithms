01939b768c3f2c50ead401acb5dab02db72f5e44
Sebastian Tleye <sebastian.tleye@avature.net>
2018-09-08 15:13:26 -0300

    Improved BubbleSort

--- BubbleSort.swift
+++ BubbleSort.swift
@@ -2,16 +2,13 @@ extension Array where Element : Comparable {
 
     mutating func bubbleSort(by areInIncreasingOrder: @escaping (Element, Element) -> Bool) {
         if self.count <= 1 { return }
-        var isOrdered: Bool = true
-        repeat {
-            isOrdered = true
-            for i in 0..<self.count-1 {
-                if areInIncreasingOrder(self[i+1], self[i]) {
-                    swapAt(i, i+1)
-                    isOrdered = false
+        for i in stride(from: self.count-1, to: 1, by: -1) {
+            for j in 0..<i {
+                if areInIncreasingOrder(self[j+1], self[j]) {
+                    swapAt(j, j+1)
                 }
             }
-        } while !isOrdered
+        }
     }
 
 }
